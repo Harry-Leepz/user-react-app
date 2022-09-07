7cf82b6b58556313638873e17f463951c59af4fa
Harry Dhillon <hdhillon478@gmail.com>
2022-09-07 05:53:35 +0100

    Add import and className to Card component

--- src/components/Ui/Card.js
+++ src/components/Ui/Card.js
@@ -1,7 +1,7 @@
-import React from "react";
+import styles from "./Card.module.css";
 
 const Card = () => {
-  return <div>Card</div>;
+  return <div className={styles.card}>Card</div>;
 };
 
 export default Card;
