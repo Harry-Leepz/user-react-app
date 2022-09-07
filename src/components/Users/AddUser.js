4a8b8fe64efaa4a73e8a8f696ea2491dffcee723
Harry Dhillon <hdhillon478@gmail.com>
2022-09-07 05:44:03 +0100

    Add logic to handle AddUser form submission

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,6 +1,11 @@
 const AddUser = () => {
+  // prevent page reload on form submission
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <form>
+    <form onSubmit={onSubmitHandler}>
       <label htmlFor='username'>Username:</label>
       <input id='username' type='text' />
       <label htmlFor='age'>Age:</label>
