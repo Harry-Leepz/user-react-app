9345dc361c29f4307162329dbb6e10b8d658c3bd
Harry Dhillon <hdhillon478@gmail.com>
2022-09-09 21:44:38 +0100

    Add logic to manage user input in state

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,21 +1,36 @@
+import { useState } from "react";
 import Button from "../Ui/Button";
 import Card from "../Ui/Card";
 
 import styles from "./AddUser.module.css";
 
 const AddUser = () => {
+  // state
+  const [usernameInput, setUsernameInput] = useState("");
+  const [ageInput, setAgeInput] = useState("");
+
   // prevent page reload on form submission
   const onSubmitHandler = (event) => {
     event.preventDefault();
+    console.log("Username is", usernameInput);
+    console.log("Age is", ageInput);
+  };
+
+  const usernameChangeHandler = (event) => {
+    setUsernameInput(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setAgeInput(event.target.value);
   };
 
   return (
     <Card className={styles.input}>
       <form onSubmit={onSubmitHandler}>
         <label htmlFor='username'>Username:</label>
-        <input id='username' type='text' />
+        <input id='username' type='text' onChange={usernameChangeHandler} />
         <label htmlFor='age'>Age:</label>
-        <input id='age' type='number' />
+        <input id='age' type='number' onChange={ageChangeHandler} />
         <Button type={"submit"}>Add User</Button>
       </form>
     </Card>
