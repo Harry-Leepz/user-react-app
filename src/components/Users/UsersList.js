71adcf069241e525d7e8f5ba3907d593bad3236d
Harry Dhillon <hdhillon478@gmail.com>
2022-09-11 21:57:10 +0100

    Fix issue with array.map method needing to return

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -2,9 +2,11 @@ const UsersList = (props) => {
   return (
     <ul>
       {props.users.map((user) => {
-        <li>
-          {user.name} ({user.age} years old)
-        </li>;
+        return (
+          <li>
+            {user.name} ({user.age} years old)
+          </li>
+        );
       })}
     </ul>
   );
